476bd5a08810c5e061006a995431f9c72a69287f
Leonardo T. S <leonardo_takanobu@hotmail.com>
2023-04-18 19:06:41 -0300

    add: custom exception for db's failure

--- src/modules/common/filters/prisma-error/prisma-error.filter.ts
+++ src/modules/common/filters/prisma-error/prisma-error.filter.ts
@@ -4,28 +4,34 @@ import {
   ConflictException,
   ExceptionFilter,
   HttpException,
-  InternalServerErrorException,
 } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
-import { Request, Response } from "express";
+import { Response } from "express";
+import { DatabaseException } from "../../exceptions/database.exception";
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaErrorFilter implements ExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const code = exception.code;
+    const prismaErrorCode = exception.code;
 
     let error: HttpException;
 
-    switch (code) {
+    switch (prismaErrorCode) {
       case "P2002":
         error = new ConflictException();
         return response.status(error.getStatus()).json(error.getResponse());
       default:
-        error = new InternalServerErrorException();
-        return response.status(error.getStatus()).json(error.getResponse());
+        error = new DatabaseException();
+        const errorResponse = error.getResponse();
+        return response
+          .status(error.getStatus())
+          .json(
+            typeof errorResponse === "object"
+              ? { ...errorResponse, errorCode: prismaErrorCode }
+              : error.getResponse(),
+          );
     }
   }
 }
